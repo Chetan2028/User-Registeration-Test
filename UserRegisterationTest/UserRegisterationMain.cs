d2829ae096fe1939844b7471a7ea7e31ae97415a
Chetan2028 <bmchetan2028@gmail.com>
2020-11-05 07:52:30 +0530

    Add project files.

---
+++ UserRegisterationTest/UserRegisterationMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserRegisterationTest
+{
+    class UserRegisterationMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registeration");
+            UserValidator userValidator = new UserValidator();
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine(userValidator.ValidateFirstName(firstName));
+        }
+    }
+}
