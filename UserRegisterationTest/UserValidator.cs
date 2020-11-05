fdb4ece0d74427384dc16ec910c7b0f6e3115dc1
Chetan2028 <bmchetan2028@gmail.com>
2020-11-05 14:37:54 +0530

    [Chetan] Add . Added to check for atleast one special character

--- UserRegisterationTest/UserValidator.cs
+++ UserRegisterationTest/UserValidator.cs
@@ -9,7 +9,7 @@ namespace UserRegisterationTest
     {
         //Constants
         public const string REGEX_NAME = "^[A-Z][a-zA-Z]{2,}$";
-        public const string REGEX_EMAIL = "^[a-z]{3,}[.][a-z]*[@][a-z]{2}[.][a-z]{2}[.][a-z]*$";
+        public const string REGEX_EMAIL = "^[a-z]{3,}[.][a-z]*[@][a-z]{2,}[.][a-z]{2,}[.][a-z]*$";
         public const string REGEX_MOBILE_NUMBER = "^[0-9]{2}[ ][0-9]{10}$";
         public const string REGEX_PASSWORD = @"^(?=.*[!@#$%^&*]){1}(?=.*\d)(?=.*[A-Z]).{8,}$";
 
@@ -62,5 +62,6 @@ namespace UserRegisterationTest
         {
             return Regex.IsMatch(password, REGEX_PASSWORD);
         }
+
     }
 }
