c4bde0f87890e9d57ecb76767b656864009f9632
Chetan2028 <bmchetan2028@gmail.com>
2020-11-05 14:40:13 +0530

    [Chetan] Add . Added to check for sample emails

--- UserRegisterationTest/UserValidator.cs
+++ UserRegisterationTest/UserValidator.cs
@@ -9,7 +9,7 @@ namespace UserRegisterationTest
     {
         //Constants
         public const string REGEX_NAME = "^[A-Z][a-zA-Z]{2,}$";
-        public const string REGEX_EMAIL = "^[a-z]{3,}[.][a-z]*[@][a-z]{2}[.][a-z]{2}[.][a-z]*$";
+        public const string REGEX_EMAIL = "^[a-z]{3,}[-.+]?[0-9]*[@][a-z0-9]*[.][a-z]*[,]?[.]?[a-z]*$";
         public const string REGEX_MOBILE_NUMBER = "^[0-9]{2}[ ][0-9]{10}$";
         public const string REGEX_PASSWORD = @"^(?=.*[!@#$%^&*]){1}(?=.*\d)(?=.*[A-Z]).{8,}$";
 
